21c8586316c5bfb876dade8ad64a09caa6ebd63b
Ural Bayhan <uralbayhan0@gmail.com>
2019-10-13 00:44:40 +0200

    Added saving in popup window

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,10 @@
-flagDB.open(function() {});
+var dirty = false;
 var titleElement = document.getElementById("title");
 var textBoxElement = document.getElementById("siteFlagTextArea");
 var gettingCurrent = browser.windows.getCurrent({populate: true});
+var currentUrl = "";
 
 gettingCurrent.then(function(windowInfo) {
-    
-    var currentUrl = "";
     for (var i = 0; i < windowInfo.tabs.length; i++) {
         var tabInfo = windowInfo.tabs[i];
         if (tabInfo.active) {
@@ -22,3 +21,22 @@ gettingCurrent.then(function(windowInfo) {
 }, function (error) {
     titleElement.innerText = "error" + error;
 });
+
+textBoxElement.oninput = function() {
+    dirty = true;
+}
+
+window.onunload = function() {
+    save(currentUrl, textBoxElement.value);
+} 
+
+function save(url, comment) {
+    if (dirty) {
+        var sending = browser.runtime.sendMessage({url: url, comment:comment, type: "update"});
+        sending.then(function(message) {
+            // TODO maybe show a "saving" label or smt
+        }, function(error) {
+            // TODO maybe some label here or alert the user the changes couldn't be saved
+        });
+    }
+}
